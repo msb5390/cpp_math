df53a43cebf2a7dbc21c8cbe4283b6d49bfb01f0
Michael Bell <msb5390@gmail.com>
2019-12-22 22:14:12 -0600

    Committing other directories.

---
+++ include/bisection.h
@@ -0,0 +1,9 @@
+// bisection.h
+//
+// Header file for the bisection function.
+//
+// Written by Michael S. Bell
+// msb5390@gmail.com
+// December 22, 2019
+
+double bisection(double f(double), double a, double b, double accuracy);
